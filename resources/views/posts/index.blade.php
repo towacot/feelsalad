b3290a37954563ae4cc731ecfbf21a1b9ece0a89
User Towacott <towacott@www2497.sakura.ne.jp>
2024-12-10 12:56:17 +0900

    date hyouji

--- resources/views/posts/index.blade.php
+++ resources/views/posts/index.blade.php
@@ -16,7 +16,7 @@
         @if (!empty($posts) && count($posts) > 0)
             @foreach ($posts as $post)
                 <a class="post_card" href="{{route('posts.show',['id' => $post->id]) }}">
-                    <div class="post_timestamp">{{$post->created_at}}</div>
+                    <div class="post_timestamp">{{ $post->created_at->format('Y-m-d') }}</div>
                     <div class="post_title">{{$post->title}}</div>
                     <div class="post_description">{{ strip_tags(Str::limit( $post->description,150,'…')) }}</div>
                 </a>
