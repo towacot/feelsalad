3ab786abb77a6b6553c634f0548573f4f384f734
User Towacott <towacott@www2497.sakura.ne.jp>
2024-12-11 22:28:15 +0900

    temp henkou

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,3 @@ use App\Http\Controllers\PostController;
 // ルート定義の整理
 Route::get('/', [PostController::class, 'index'])->name('posts.index');
 Route::get('/show/{id}', [PostController::class, 'show'])->name('posts.show');
-
-
-//これをしないと管理画面が見えなくなる。authに分けたりはしないでいいらしい。
-//これまだ必要？atode
-Livewire::setUpdateRoute(function ($handle) {
-    return Route::post('/livewire/update', $handle);
-});
