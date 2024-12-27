06dc8a9d1ceec6e5a013be7081acc7aff431b5a0
towacott <snakt@outlook.jp>
2024-12-27 14:53:24 +0900

    show date

--- resources/views/posts/show.blade.php
+++ resources/views/posts/show.blade.php
@@ -9,7 +9,7 @@
         @endif
         <div class="show_container">
             <div class="show_title">{{ $post->title }}</div>
-            <div class="show_timestamp">{{ $post->created_at }}</div>
+            <div class="show_timestamp">{{ $post->created_at->format('Y-m-d') }}</div>
             <div class="show_description">{!! html_entity_decode($post->description) !!}</div>
             {{--文章で画像もいっしょにやりたい--}}
             @if ($post->image)
